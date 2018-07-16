b7940c8664fc3927824aad127dd20d080da63f05
Gabo <gabo141298@gmail.com>
2018-07-15 20:50:46 -0600

    Creation of the project. Added some files that we can adapt for our program.

---
+++ src/Game.h
@@ -0,0 +1,42 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <QApplication>
+
+class QGraphicsScene;
+class QSvgRenderer;
+class QGraphicsView;
+class Player;
+class Score;
+
+/**
+	Game controller. An object that represents the game.
+*/
+class Game : public QApplication
+{
+	Q_DISABLE_COPY(Game)
+
+  protected:
+	// Manages graphic items. It is an invisible manager
+	QGraphicsScene* scene;
+	/// A visible widget that renders the scene
+    QGraphicsView* view;
+	/// Shows the score on the screen to the player
+	Score* score;
+	/// Parsers the assets svg file
+	QSvgRenderer* svgRenderer = nullptr;
+
+  public:
+	/// Constructor
+	Game(int &argc, char **argv, int flags = ApplicationFlags);
+	/// Destructor
+	~Game();
+	/// Call this method instead of exec()
+	int run();
+	/// Play the given filename as background music
+	void playBackgroundMusic(const QString& audioFilename);
+
+  protected slots:
+};
+
+#endif // GAME_H
