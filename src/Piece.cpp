d979c2514b308987e75de9619f0dc45a1aabc239
Jostin Alvarez <jostin.alvarez@ucr.ac.cr>
2018-07-18 00:33:28 -0600

    Added a validation to the isEnemy method. Now it checks that the cell is not out of boundaries.

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -4,17 +4,25 @@
 
 bool Piece::isEnemy( short row, short file)
 {
+    // If it is out of boundaries
+    if ( row < 0 || row > 7 || file < 0 || file > 7)
+        return false;
+
     // If the piece is black, check if the cell is upper case
     if ( islower(this->symbol) )
         return isupper(board[row][file]->getSymbol());
 
+    // Return true if the piece in the cell is the opposite color to this
     return islower(this->board[row][file]->getSymbol());
 }
 
 bool Piece::isFree( short row, short file)
 {
+    // If the cell is out of boundaries, it cannot be free
 	if ( row < 0 || row > 7 || file < 0 || file > 7)
 		return false;
+
+    // Return true if the cell is not occupied, 
 	return (this->board[row][file] == nullptr);
 }
 
